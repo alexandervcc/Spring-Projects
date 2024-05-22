a9c33d1ac056d007b33de8ee4ab0560e0c4d9788
alexandervcc <alex.charco.25@gmail.com>
2024-05-21 22:20:08 -0500

    Base Spring project

---
+++ oauth2/server/src/test/java/acc/oauth2/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.oauth2.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
